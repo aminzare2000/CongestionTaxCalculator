2d97037862e994cdeff04e35265b4a7f1236c9b7
aminzare <amin.zare@gmail.com>
2023-07-11 18:01:00 +0330

    Add IRepository and ITariffRepository

---
+++ src/CongestionTaxCalculator.Domain/Persistence/InterfaceRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongestionTaxCalculator.Domain.Persistence.InterfaceRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<T?> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T?> Delete(int id);
+    }
+}
---
+++ src/CongestionTaxCalculator.Domain/Persistence/InterfaceRepository/ITariffRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongestionTaxCalculator.Domain.Persistence.InterfaceRepository
+{
+    public interface ITariffDefinitionRepository : IRepository<TariffDefinition>
+    {
+        IEnumerable<TariffDefinition> GetTariff(int CityId, int StartTariffYear, int TariffNO);
+    }
+}
